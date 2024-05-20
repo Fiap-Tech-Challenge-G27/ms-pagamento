ba54632b17275c889f69a003ca851160cbbcfd24
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-05-20 10:08:49 -0300

    cicd

--- dist/modules/payment/controller/payment.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PaymentConfirmationDto } from '../dtos/payment-confirmation.dto';
-import { ConfirmatePaymentUseCase } from '../use-cases/confirmate-payment.usecase';
-export declare class PaymentController {
-    private readonly confirmatePaymentUseCase;
-    constructor(confirmatePaymentUseCase: ConfirmatePaymentUseCase);
-    receivePaymentConfirmation(payment_confirmation: PaymentConfirmationDto): Promise<void>;
-}
--- dist/modules/payment/core/payment-gateway.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IPaymentGateway = void 0;
-class IPaymentGateway {
-}
-exports.IPaymentGateway = IPaymentGateway;
-//# sourceMappingURL=payment-gateway.js.map
